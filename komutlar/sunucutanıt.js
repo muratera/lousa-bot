351226305e7ba7fcab5c1164a1d6dfa5826b1c4d
Glitch (hello-express) <none>
2019-01-12 10:36:52 +0000

    🍤🍄 Checkpoint ./komutlar/sunucutanıt.js:2426017/15976 ./sunucutanıt.json:2426017/10 ./package.json:2426017/1914

--- komutlar/sunucutanıt.js
+++ komutlar/sunucutanıt.js
@@ -1,30 +1,66 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
+const fs = require('fs');
+const ms = require("ms")
+const db = require('quick.db')
 exports.run = async (client, message, args) => {
-    var channel = client.channels.find('name', 'sunucutanit-komutu') 
-    const asdf = await client.channels.get(message.channel.id).createInvite()
-    message.delete();
-    const embed = new Discord.RichEmbed()
-        .setTitle("» Rosemary | Sunucu Tanıt")
-        .setDescription("**Sunucu tanıtılmıştır. https://discord.gg/zKXnTDP tıklayarak sunucunu tanıttığın sunucuya katılabilirsin.**")
-        .setFooter(" Righter 2018")
-    message.channel.send(embed)
-    const invite = new Discord.RichEmbed()
-        .setAuthor("» Rosemary | Sunucu Tanıt")
-        .addField('**» Tanıtan: **', message.author.username + '#' + message.author.discriminator)
-        .addField('**» Sunucu Adı: **', message.guild.name)
-        .setDescription(asdf.url)
-    channel.send(invite)
+  	if (!message.guild) {
+    const ozelmesajuyari = new Discord.RichEmbed()
+    .setColor(0xFF0000)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField(':warning: Uyarı :warning:', '`+sunucutanıt` adlı komutu özel mesajlarda kullanamazsın.')
+    return message.author.sendEmbed(ozelmesajuyari); }
+    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("Bu Komutu kullanmanız için `Sunucu_Yönet` Yetkisine sahip olmalısınız.")
+    let kullanildii = JSON.parse(fs.readFileSync('./sunucutanıt.json', 'utf8'));
+  if (!kullanildii[message.guild.id]) kullanildii[message.guild.id] = {
+    gunlukkullanim: 0
+  }
+  if (kullanildii[message.guild.id].gunlukkullanim == 0)
+  {
+        const embed = new Discord.RichEmbed()
+  .setTitle('BAŞARILI')
+  .setDescription('Sunucu [burada](https://discord.gg/zKXnTDP tanıtıldı! \n\n 12 saat Sonra sunucunuzu Tekrardan Tanıtabilirsiniz. \n\n Sunucunu tanıtabilmek için beni [ekle!](https://discordapp.com/oauth2/authorize?client_id=525313553734041600&scope=bot&permissions=2146958847)')
+  .setColor('GREEN')
+ message.channel.sendEmbed(embed);
+    message.channel.createInvite({maxAge: 0}).then((invite) => {
+        const embed = new Discord.RichEmbed()
+            .addField(` Sunucu Sahibi`, message.author.tag, true)
+            .addField(` Sunucu İsmi`, message.guild.name, true)
+      .addField(` Sunucudakı Üye Sayısı`, message.guild.members.size, true)
+      .addField(` Sunucu Davet Linki`, invite.url, true)
+            .setColor('RANDOM')
+      .setThumbnail(message.guild.iconURL)
+       client.channels.get('533408512076677120').send(embed)
+            });
+  kullanildii[message.guild.id].gunlukkullanim = 1
+    
+  fs.writeFile('./sunucutanıt.json', JSON.stringify(kullanildii), (err) => {
+      if (err) console.error(err)
+    })
+  return
+  }
+  setTimeout(async() => {
+    kullanildii[message.guild.id].gunlukkullanim = 0
+    fs.writeFile('./sunucutanıt.json', JSON.stringify(kullanildii), (err) => {
+      if (err) console.error(err)
+    })
+  }, ms('12h'));
+  
+  if (kullanildii[message.guild.id].gunlukkullanim == 1)
+  {
+  message.channel.send({embed: {
+      description: '**BAŞARISIZ TANITIM** \n\nBu komut zaten kullanılmış!\n\nSunucunu 12 saate 1 defa tanıtabilirsin! \n\n[Rosemary | Discord.js | Kod Paylaşım](https://discord.gg/zKXnTDP) \n[Rosemary Ekle](https://discordapp.com/oauth2/authorize?client_id=532272597556002828&scope=bot&permissions=2146958847)'
+            }});
+  }
 };
-
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: [],
-    permLevel: 0
-};
-
+    aliases: ['sunucutanıt'],
+    permLevel: 2,
+}
 exports.help = {
-    name: 'sunucutanıt',
-    description: 'sunucutanıt',
+    name: 'sunucunutanıt',
+    description: 'Sunuzunuzu Tanıtmak İçin En Uygun Kod!',
     usage: 'sunucutanıt'
-};
+}
