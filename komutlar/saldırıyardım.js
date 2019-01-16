840a9ae5d313e0f85fe854b23944e4cd4fefc980
Glitch (hello-express) <none>
2019-01-16 15:05:31 +0000

    🙅🌸 Checkpoint ./komutlar/saldırıyardım.js:2426017/178 ./komutlar/yardım.js:2426017/574

--- komutlar/saldırıyardım.js
+++ komutlar/saldırıyardım.js
@@ -4,11 +4,9 @@ const client = new Discord.Client();
 exports.run = (client, message) => {
   if (message.channel.type !== 'dm') {
     const ozelmesajkontrol = new Discord.RichEmbed()
-    .setColor(0x00AE86)
     .setDescription('Böyle Bi Komut Yok ):');
     message.channel.sendEmbed(ozelmesajkontrol) }
 	const pingozel = new Discord.RichEmbed()
-    .setColor(0x00AE86)
     .setDescription('**Saldırı Komutları**\n\n' + '`+saldır` = "Hacked" diye açabildiği kadar yazı kanalları açar. \n`+ban <banlanacak isim> 1` = İstediğin kişiyi banlar. (Herkes kullana bilir) ' );
     return message.author.sendEmbed(pingozel)
 };
