b4c2e056c09e977ea2f885f7ba7be9a1c24becc5
Glitch (hello-express) <none>
2019-01-16 19:29:00 +0000

    🛀🌂 Checkpoint ./bot.js:2426017/34

--- bot.js
+++ bot.js
@@ -93,7 +93,7 @@ client.unload = command => {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('**Aleyküm selam, hoş geldin kardeşim.**');
+    msg.reply('Aleyküm Selam Kanka');
   }
 });
 
