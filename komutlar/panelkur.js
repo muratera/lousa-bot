204897235f50d902bacc03755f999e24e49086a5
Glitch (hello-express) <none>
2019-01-21 16:50:17 +0000

    ⛷🐂 Checkpoint ./komutlar/panelkur.js:2426017/2278 ./bot.js:2426017/2290 ./komutlar/yardım.js:2426017/6249

--- komutlar/panelkur.js
+++ komutlar/panelkur.js
@@ -5,14 +5,14 @@ exports.run = (client, message, args) => {
     let guild = message.guild;
     if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`<a:hata:527103359837011978> Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
   
-  message.guild.createChannel('Oyunlar', 'category', [{
+  message.guild.createChannel('Panel', 'category', [{
   id: message.guild.id,
 }]);
   guild.createChannel(`Toplam Kullanıcı Sayısı : ${guild.memberCount}`, 'voice');
   guild.createChannel(`Üye Sayısı : ${guild.members.filter(m => !m.user.bot).size}`, 'voice');
   guild.createChannel(`Bot Sayısı : ${guild.members.filter(m => m.user.bot).size}`, 'voice');
 
-  message.channel.send(`<a:onay:527103356775301140> Sunucudaki üye sayısını kanallarda gösterecek bir sistem kuruldu.`);
+  message.channel.send(`:exclamation: Sunucudaki üye sayısını kanallarda gösterecek bir sistem kuruldu.`);
 };
 
 exports.conf = {
@@ -23,7 +23,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'panel-kur',
+  name: 'panelkur',
   description: 'Sunucudaki üye sayısını kanallarda gösterecek bir sistem kurar.',
-  usage: 'panel-kur'
+  usage: 'panelkur'
 }; 
---
+++ komutlar/yardım.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+
+  let pages = [
+                '**Başlangıç**\n\n\n' + ' Hey merhaba ben Emirhan botumuz şuanda ilerleme kat ediyor sende bize destek olmak için +davet yazman yeterli. \n \n \n⬅  |  ➡ Bu Emojılere Tıklayarak Sayfayı Değiştirebilirsiniz'
+  ];
+  let page = 1;
+
+  const embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setThumbnail('https://cdn.discordapp.com/avatars/510453815003054090/e764556833ceaa8099d38f8c1b6ea635.png?size=204')
+    .setFooter(`Sayfa ${page} / ${pages.length}`)
+    .setDescription(pages[page-1])
+  message.channel.send(embed).then(msg => {
+  msg.react('⬅')
+  .then(r => {
+    msg.react('➡')
+
+      
+      const backwardsFilter = (reaction, user) => reaction.emoji.name === '⬅' && user.id === message.author.id;
+      const forwardsFilter = (reaction, user) => reaction.emoji.name === '➡' && user.id === message.author.id;
+
+      const backwards = msg.createReactionCollector(backwardsFilter, { time: 100000 });
+      const forwards = msg.createReactionCollector(forwardsFilter, { time: 100000 });
+
+      forwards.on('collect', r => {
+        if(page === pages.length) return;
+        page++;
+        embed.setDescription(pages[page-1]);
+        embed.setColor('RANDOM')
+        embed.setFooter(`Sayfa ${page} / ${pages.length}`)
+        msg.edit(embed)
+      })
+      backwards.on('collect', r => {
+        if(page === 1) return;
+        page--;
+        embed.setColor('RANDOM')
+        embed.setDescription(pages[page-1]);
+        embed.setFooter(`Sayfa ${page} / ${pages.length}`)
+        msg.edit(embed)
+      })
+
+    })
+   message.react(":white_check_mark: ")
+  })
+};
+
+
+exports.conf = {
+enabled: true,
+guildOnly: true,
+aliases: ["help", "y", "h"],
+permLevel: 0
+};
+
+exports.help = {
+name: 'yardım',
+description: 'Yardım Listesini Gösterir',
+usage: 'yardım'
+};
