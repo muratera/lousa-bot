5e7629e79dc1b0e97ada0d5c2fb675be32960349
Glitch (hello-express) <none>
2019-02-11 19:25:51 +0000

    🚅💫 Checkpoint ./komutlar/panelkur.js:2426017/3122

--- komutlar/panelkur.js
+++ komutlar/panelkur.js
@@ -1,19 +1,43 @@
-
-const Discord = require('discord.js');
-
-exports.run = (client, message, args) => {
-    let guild = message.guild;
-    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`<a:hata:527103359837011978> Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
-  
-  message.guild.createChannel('Panel', 'category', [{
-  id: message.guild.id,
-}]);
-  guild.createChannel(`Toplam Kullanıcı Sayısı : ${guild.memberCount}`, 'voice');
-  guild.createChannel(`Üye Sayısı : ${guild.members.filter(m => !m.user.bot).size}`, 'voice');
-  guild.createChannel(`Bot Sayısı : ${guild.members.filter(m => m.user.bot).size}`, 'voice');
-
-  message.channel.send(`:exclamation: Sunucudaki üye sayısını kanallarda gösterecek bir sistem kuruldu.`);
-};
+const Discord = require('discord.js')
+const db = require('quick.db')
+exports.run = async (client, message, args) => {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
+  var sunucupanel = message.guild.createChannel("» Sunucu Panel", "category").then(sp => {
+	var toplamkullanıcı =  message.guild.createChannel(`Toplam Kullanıcı Sayısı: ${message.guild.memberCount}`, "voice").then(ss => {
+        ss.setParent(sp)
+     				db.set(`toplamkullanıcı_${message.guild.id}` , ss.id)
+                   let role = message.guild.roles.find(a => a.name ===  "@everyone");
+        ss.overwritePermissions(role, {
+            CONNECT: false,
+        });
+})
+var toplamkişi =  message.guild.createChannel(`Toplam Kişi Sayısı: ${message.guild.members.filter(m => !m.user.bot).size}`, "voice").then(ss => {
+	ss.setParent(sp)
+				db.set(`toplamkişi_${message.guild.id}` , ss.id)
+			   let role = message.guild.roles.find(a => a.name ===  "@everyone");
+	ss.overwritePermissions(role, {
+		CONNECT: false,
+	});
+})
+var toplambot =  message.guild.createChannel(`Toplam Bot Sayısı: ${message.guild.members.filter(m => m.user.bot).size}`, "voice").then(ss => {
+	ss.setParent(sp)
+				 db.set(`toplambot_${message.guild.id}` , ss.id)
+			   let role = message.guild.roles.find(a => a.name ===  "@everyone");
+	ss.overwritePermissions(role, {
+		CONNECT: false,
+	});
+})
+var banlı = message.guild.fetchBans().then(bans => message.guild.createChannel(`Toplam Banlı Sayısı: ${bans.size}`, "voice")).then(ss => {
+	ss.setParent(sp)
+				 db.set(`banlı_${message.guild.id}` , ss.id)
+			   let role = message.guild.roles.find(a => a.name ===  "@everyone");
+	ss.overwritePermissions(role, {
+		CONNECT: false,
+	});
+})
+  })
+  message.channel.send(`Panel kuruldu.`);
+}
 
 exports.conf = {
   enabled: true,
