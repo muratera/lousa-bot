d30ab667dd053be96f4b2264aec1762ffce2b878
Glitch (hello-express) <none>
2019-03-01 18:00:16 +0000

    ❤️🤑 Checkpoint ./komutlar/davet.js:2426017/710 ./bot.js:2426017/8271 ./events/ready.js:2426017/115

--- events/ready.js
+++ events/ready.js
@@ -10,12 +10,8 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı! BOT aktif!`);
   client.user.setStatus("online");
    var oyun = [
-        "",
-        "",
-        "",
-     
-        ``,
-        ``
+        "/yardım | /davet",
+        "Sürüm: 0.0.2",
 
     ];
 
