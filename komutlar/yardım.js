3d5802e8e280f0706c0fe49de4e5da9706edacab
Glitch (hello-express) <none>
2019-03-26 14:35:28 +0000

    💪💏 Checkpoint ./komutlar/yardım.js:2426017/2961

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -3,19 +3,16 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
 
   let pages = [
-              '**Kurulum**\n\n\n ' + '+**genelsw =** Genel Discord Sunucusu Kurar \n+**oyunsw =** Oyun Discordu Sunucusu Kurar',
+              '**Kurulum**\n\n ' + '/**genelsw =** Genel Discord Sunucusu Kurar \n/**oyunsw =** Oyun Discordu Sunucusu Kurar',
               ];
   let page = 1;
 
   const embed = new Discord.RichEmbed()
     .setColor('RANDOM')
     .setThumbnail('https://cdn.discordapp.com/avatars/510453815003054090/e764556833ceaa8099d38f8c1b6ea635.png?size=204')
-    .setFooter(`Sayfa ${page} / ${pages.length}`)
+    .setFooter(`Yapımcı Emirhan Cem`)
     .setDescription(pages[page-1])
   message.channel.send(embed).then(msg => {
-  msg.react('⬅')
-  .then(r => {
-    msg.react('➡')
 
       
       const backwardsFilter = (reaction, user) => reaction.emoji.name === '⬅' && user.id === message.author.id;
@@ -24,26 +21,7 @@ exports.run = (client, message, args) => {
       const backwards = msg.createReactionCollector(backwardsFilter, { time: 100000 });
       const forwards = msg.createReactionCollector(forwardsFilter, { time: 100000 });
 
-      forwards.on('collect', r => {
-        if(page === pages.length) return;
-        page++;
-        embed.setDescription(pages[page-1]);
-        embed.setColor('RANDOM')
-        embed.setFooter(`Sayfa ${page} / ${pages.length}`)
-        msg.edit(embed)
-      })
-      backwards.on('collect', r => {
-        if(page === 1) return;
-        page--;
-        embed.setColor('RANDOM')
-        embed.setDescription(pages[page-1]);
-        embed.setFooter(`Sayfa ${page} / ${pages.length}`)
-        msg.edit(embed)
-      })
-
     })
-   message.react(":white_check_mark: ")
-  })
 };
 
 
