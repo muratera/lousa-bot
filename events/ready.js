660fb1d192d7f7fbb808330ddc3e8ff5c30fbf45
Glitch (hello-express) <none>
2019-04-04 12:50:59 +0000

    🛤👩‍👩‍👦 Checkpoint ./events/ready.js:2426017/8

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı! BOT aktif!`);
   client.user.setStatus("online");
    var oyun = [
-        "-js",
+        "-js | -html",
 
 
     ];
