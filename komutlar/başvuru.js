59a4b2790e6f973b78de20ed551735c3c0dfa853
Glitch (hello-express) <none>
2019-04-06 23:13:48 +0000

    🍅😄 Checkpoint ./komutlar/başvuru.js:2426017/1752

--- komutlar/başvuru.js
+++ komutlar/başvuru.js
@@ -1,37 +1,23 @@
-
 const Discord = require('discord.js');
 
-exports.run = (client, message, args) => {
-  let reason = args.slice(1).join(' ');
-  let guild = message.guild
-  let terfiler = guild.channels.find('name', 'yetkili-bot-chat');
-  if (!terfiler) return message.reply('`yetkili-bot-chat` kanalını bulamıyorum.');
-  let user = message.mentions.users.first();
-  if (reason.length < 1) return message.reply('ne dilleribiliyosunuz ve kendinizi etiketleyin.');
-  if (message.mentions.users.size < 1) return message.reply('Isminizi Etiketleyin.').catch(console.error);
-  const embed = new Discord.RichEmbed()
-  
-    .setColor(0xD97634)
-	.setThumbnail("https://i.hizliresim.com/mJ20o2.jpg")
-    .setTimestamp()
-    .addField('Durum:', 'Bekleniyor')
-    .addField('Gonderen Kisi:', `${user.username}#${user.discriminator} (${user.id})`)
-    .addField('Bilgiler', reason);
-	   message.channel.send(`:white_check_mark: Başvurunuz alındı.`)  
-    message.delete(10000)
+const cevaplar = [    "https://forms.gle/MuVWvxbtmPT3eWZL7"];
 
-	return guild.channels.get(terfiler.id).sendEmbed(embed);
-};
+exports.run = function(client, message, args) {
+    var soru = args.join(' ');
+
+    var cevap = cevaplar[Math.floor(Math.random() * cevaplar.length)];
+    message.channel.send(cevap)
+};  
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['basvuru'],
-  permLevel: 0
+  enabled: true, 
+  guildOnly: true, 
+  aliases: ["link","başvurulink"],
+  permLevel: 0 
 };
 
 exports.help = {
-  name: 'başvuru',
-  description: 'Kullanıcıyı terfi ettirir.',
-  usage: 'başvuru [kullanıcı]'
+  name: 'başvuru', 
+  description: '',
+  usage: ''
 };
