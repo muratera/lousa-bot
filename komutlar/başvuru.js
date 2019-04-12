d4f65ff72693032f093ac0b5514d6d390c3869b5
Glitch (hello-express) <none>
2019-04-12 16:42:32 +0000

    🌊🌓 Checkpoint ./komutlar/başvuru.js:2426017/313

--- komutlar/başvuru.js
+++ komutlar/başvuru.js
@@ -3,18 +3,18 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
   let reason = args.slice(1).join(' ');
   let guild = message.guild
-  let terfiler = guild.channels.find('name', 'yetki-başvurusu');
-  if (!terfiler) return message.reply('`yetki-başvurusu` kanalını bulamıyorum.');
+  let terfiler = guild.channels.find('name', 'başvuru');
+  if (!terfiler) return message.reply('`başvuru` kanalını bulamıyorum.');
   let user = message.mentions.users.first();
-  if (reason.length < 1) return message.reply('Ne Kazandirirsiniz,Istediniz Yetki,Kac Saat Aktif Olacaginiz,Adiniz,Yasiniz ve Kendinizi Etiketleyin.');
+  if (reason.length < 1) return message.reply('Ne Dilleri Biliyosunuz? (Kodlama)');
   if (message.mentions.users.size < 1) return message.reply('Isminizi Etiketleyin.').catch(console.error);
   const embed = new Discord.RichEmbed()
     .setColor(0xD97634)
 	.setThumbnail("https://i.hizliresim.com/mJ20o2.jpg")
     .setTimestamp()
     .addField('Durum:', 'Bekleniyor')
-    .addField('Gonderen Kisi:', `${user.username}#${user.discriminator} (${user.id})`)
-    .addField('Bilgiler', reason);
+    .addField('Gonderen Kisi:', `${message.author}`)
+    .addField('Dilleri', reason);
 	
 	return guild.channels.get(terfiler.id).sendEmbed(embed);
 };
