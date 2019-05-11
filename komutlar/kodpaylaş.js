faf4b2a02fab376e35a7534cb29d716ba5cec046
Glitch (hello-express) <none>
2019-05-11 19:03:17 +0000

    🏡🌺 Checkpoint ./komutlar/kodpaylaş.js:2426017/315

--- komutlar/kodpaylaş.js
+++ komutlar/kodpaylaş.js
@@ -1,7 +1,18 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-  let reason = args.slice(0).join(' '); 
+	let botisim = args[0]
+  let sahip = args[1]
+  let sebep = args[2]
+  let sebep1 = args[3]
+  let sebep2 = args[4]
+  let sebep3 = args[5]
+  let sebep4 = args[6]
+  let sebep5 = args[7]
+  let sebep6 = args[8]
+  let sebep7 = args[9]
+  let sebep8 = args[10]
+  let sebep9 = args[11]
  if (reason.length < 1) return message.reply('Ne dil bildiğinizi yazmalısnız.');
   const embed = new Discord.RichEmbed()
     .setColor(0xD97634)
