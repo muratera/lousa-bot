4f8c19b349e50c9915141bbe2f6bd2a40b77ddea
Glitch (hello-express) <none>
2019-05-16 19:35:21 +0000

    🏇⏰ Checkpoint ./komutlar/başvuru2.js:2426017/83

--- komutlar/başvuru2.js
+++ komutlar/başvuru2.js
@@ -4,8 +4,8 @@ const Discord = require('discord.js');
 exports.run = function(client, message, args) {
 
   let reason = args.slice(0).join(' ');
-  let basvuru = "556474818816376832"// başvurunun gideceği kanal
-	let kanal = "556472406680535101" // başvurunun yapılacağı kanal
+  let basvuru = "568920569093816345"// başvurunun gideceği kanal
+	let kanal = "539169862996590610" // başvurunun yapılacağı kanal
 	
   if (message.channel.id !== kanal) return message.channel.send(`Bu komutu sadece <#${kanal}> kanalında kullanabilirsin.`).then(msg => msg.delete(10000))
 	if (message.channel.id == kanal) {
@@ -15,7 +15,7 @@ exports.run = function(client, message, args) {
   .setTitle("Başvuru")
   .addField("Başvuran Kişi", message.author.tag)
   .addField("Başvuran Kişinin ID", message.author.id)
-  .addField(" ", botid)
+  .addField("Diller", reason)
   client.channels.get(basvuru).send(embed)
   message.channel.send(`:white_check_mark: Bot ekleme isteğiniz alındı.`).then
   }
