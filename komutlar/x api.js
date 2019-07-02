ce2eb03639a87af37cf4f1b37fe966065bf393d8
Glitch (hello-express) <none>
2019-07-02 19:51:09 +0000

    🌺🎎 Checkpoint ./komutlar/x api.js:2426017/36 ./komutlar/x html.js:2426017/36 ./komutlar/x js.js:2426017/36

--- komutlar/x api.js
+++ komutlar/x api.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
   let user = message.member
   let guild = message.guild
-	let kanal = "584519785694494723" // js rolunun alınıcağı kanal
+	let kanal = "595701678360231946" // js rolunun alınıcağı kanal
   
   if (message.channel.id !== kanal) return message.channel.send(`Bu komutu sadece https://discord.gg/qWDXCTD Sunucusunda Kullanabilirsin!`)
 	if (message.channel.id == kanal) {  
