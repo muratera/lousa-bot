5b2c54f3e4289d1861bcd7e8b1e70941123e7232
Glitch (hello-express) <none>
2019-07-03 00:31:11 +0000

    🗾🏁 Checkpoint ./komutlar/Rol Uyarı.js:2426017/483

--- komutlar/Rol Uyarı.js
+++ komutlar/Rol Uyarı.js
@@ -6,7 +6,12 @@ exports.run = (client, message, args) => {
 message.channel.send
 (`
 Maalasef Sunucumuzda Roller Böyle Alınmıyor! 
+
 **Peki Nasıl Alıcam?**
+http://bit.ly/kodal Sitesine Girp Yukardan Ne Kodu Alıcaksan Onun İsmine Tıkla.O Kisimda Aşağıda **Kod Al** Butonuna Tıkla Ekranda Çıkan Kodu Kopyala Ve <#595701678360231946> Kanalına "-Aldığın Kod" Şeklinde Rolünü Albilirsin
+
+**Hiçbişey Anlamadım Amk Bidaha Anlat**
+http://bit.ly/kodukullan Kanki Direk Bu Videoyu İzle
 
 `)
   
@@ -15,10 +20,10 @@ Maalasef Sunucumuzda Roller Böyle Alınmıyor!
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["js","api","html"],
+  aliases: ["js"],
   permLevel: 4
 };
 
 exports.help = {
-  name: 'botdeğiştir',
+  name: 'html',
 };
