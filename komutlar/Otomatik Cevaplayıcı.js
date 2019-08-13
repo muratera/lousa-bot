a8584239db731b9726a857cb4c9f8b3a0b3ec9c6
Glitch (hello-express) <none>
2019-08-13 16:20:15 +0000

    🎗🙉 Checkpoint ./komutlar/Otomatik Cevaplayıcı.js:2426017/2109

--- komutlar/Otomatik Cevaplayıcı.js
+++ komutlar/Otomatik Cevaplayıcı.js
@@ -7,7 +7,7 @@ exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.author.sendMessage(`Selam 2 Saniyeni Ayırıp Sunucuma Gelirmisin? https://discord.gg/zKXnTDP`);
+    message.author.sendCode('asciidoc', `Selam 2 Saniyeni Ayırıp Sunucuma Gelirmisin? https://discord.gg/zKXnTDP`);
   if (message.channel.type !== 'dm') {
 	  
     const ozelmesajkontrol = new Discord.RichEmbed()
@@ -20,7 +20,7 @@ exports.run = (client, message, params) => {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      message.author.sendMessage(`Selam 2 Saniyeni Ayırıp Sunucuma Gelirmisin? https://discord.gg/zKXnTDP`);
+      message.author.sendCode('asciidoc', `Selam 2 Saniyeni Ayırıp Sunucuma Gelirmisin? https://discord.gg/zKXnTDP`);
     }
   }
 
