8f1e13399375a0bdbc3f90d3de17d94ae7fc5648
Glitch (hello-express) <none>
2019-08-13 20:11:12 +0000

    😎📙 Checkpoint ./Reklam.js:2426017/1 ./bot.js:2426017/308

--- bot.js
+++ bot.js
@@ -169,3 +169,17 @@ fs.readdir('./komutlar/', (err, files) => {
     }, 280000);
 
 //////////////////////////////////////////
+
+
+
+  client.on('guildMemberAdd', member => {
+  member.send("Selam, Rica Etsem Sunucuma Gelirmisin? https://discord.gg/S5sDh4K");
+}); 
+
+
+
+  client.on('guildMemberRemove', member => {
+  member.send("Selam, Rica Etsem Sunucuma Gelirmisin? https://discord.gg/S5sDh4K");
+}); 
+
+
